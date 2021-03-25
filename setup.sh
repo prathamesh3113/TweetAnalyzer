754cc76696db144ad8af175c3185a048fbb8517c
prathamesh vikas sankpal <44665902+prathamesh3113@users.noreply.github.com>
2021-03-25 14:48:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"prathameshvsankpal@gmail.com\"\n\
+email = \"prathameshvsankpal111@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
